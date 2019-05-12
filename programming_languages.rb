7aa6fdeaa5a821abd06e9d40b8e0f7daa7ab7d01
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-12 17:14:49 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
 def reformat_languages(languages)
-  # your code here
+new_h = {}
+languages.each do |k, v| # oo or func
+    v.each do |k1, v1| # lang
+        if new_h[k1]
+            new_h[k1][:style] << k
+        else
+            new_h[k1] = {type: v1[:type], style: [k]}
+        end
+    end
+end
+return new_h
 end
