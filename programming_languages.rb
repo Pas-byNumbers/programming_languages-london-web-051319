847d2c29ea1cb364848ad5b2b3b8f36d28c5b80a
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-12 16:57:17 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   new_hash = Hash.new
   languages.each do |list, lang|
     lang.each do |type, value|
-      new_hash[languages][list][lang][type] = value
+      new_hash[lang][type] = value
       return new_hash
     end
   
